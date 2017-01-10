fbe0c30b06cc0e2fb8c8cba99af9fbf048fa15c8
KALMAN Christophe <Christophe.KALMAN@cpnv.ch>
2017-01-10 14:41:26 +0100

    Implements auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,8 @@
-import {User} from "../models/user";
-import {Injectable} from "@angular/core";
-import {X_ACCESS_TOKEN} from "./http.service";
-import {UserService} from "./user.service";
-import {Observable} from "rxjs";
+import { User } from "../models/user";
+import { Injectable } from "@angular/core";
+import { X_ACCESS_TOKEN } from "./http.service";
+import { UserService } from "./user.service";
+import { Observable } from "rxjs";
 
 
 export let USER_PROFILE = "profile";
@@ -10,33 +10,37 @@ export let USER_PROFILE = "profile";
 @Injectable()
 export class AuthService {
 
+  constructor(public userService: UserService) { }
 
-  constructor(public userService: UserService){}
   /**
-   *
+   * 
    * @param key QrCode key
    */
-  login(key: string): Observable<User>{
+  login(key: string): Observable<User> {
     localStorage.setItem(X_ACCESS_TOKEN, key);
-    return new Observable<User>(observer =>{
-      this.userService.me().subscribe(user =>{
-        if(user){
+
+    return new Observable<User>(observer => {
+
+      this.userService.me().subscribe(user => {
+        if (user) {
           observer.next(user);
           localStorage.setItem(USER_PROFILE, JSON.stringify(user));
-        }else{
+        } else {
           observer.error("Authentification failed");
         }
         observer.complete();
+      }, error => {
+        observer.error(error);
       });
     });
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem(USER_PROFILE);
     localStorage.removeItem(X_ACCESS_TOKEN);
   }
 
-  isAuthenticated(): boolean{
+  isAuthenticated(): boolean {
     let user = localStorage.getItem(USER_PROFILE);
     return user != null;
   }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,18 @@
-import {Injectable} from '@angular/core';
-import {Response, Http} from "@angular/http";
-import {User} from "../models/user";
-import {Observable} from "rxjs";
-import {HttpService} from "./http.service";
+import { Injectable } from '@angular/core';
+import { Response } from "@angular/http";
+import { User } from "../models/user";
+import { Observable } from "rxjs";
+import { HttpService } from "./http.service";
 
 @Injectable()
 export class UserService {
   constructor(public http: HttpService) {
   }
 
-  getUser(id: string): Observable<User> {
-    return null;
+  getUser(id: number): Observable<User> {
+    return this.http.get('/api/users/' + id)
+      .map(this.extractData)
+      .catch(this.handleError);
   }
 
   me(): Observable<User> {
@@ -20,7 +22,6 @@ export class UserService {
   }
 
   private extractData(res: Response) {
-    console.log("response : ", res);
     let data = res.json();
     return data || {};
   }
@@ -35,7 +36,7 @@ export class UserService {
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
-    console.error(errMsg);
+    console.error("User service : ", error);
     return Observable.throw(errMsg);
   }
 }
