f513985f2237aa2d3208c47cff5f44c7ebf15317
Nolan Rigo <nolan.rigo@cpnv.ch>
2017-01-13 18:20:51 +0100

    Login Page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, Platform, ToastController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  private scannerConfig: any = {
+    preferFrontCamera: false,
+    showFlipCameraButton: true,
+    showTorchButton: true,
+    'prompt': 'Scannez le QRCode derrière votre carte',
+    formats: 'QR_CODE',
+  };
+
+  constructor(
+    private navCtrl: NavController,
+    private platform: Platform,
+    private toastCtrl: ToastController,
+    private auth: AuthService
+  ) {}
+
+
+  scan() {
+    this.platform.ready().then(_ => {
+
+      if(this.platform.is('core')) {
+        this.login('first_user');
+        return;
+      }
+
+      BarcodeScanner.scan(this.scannerConfig).then(result => {
+        if(result.cancelled) return;
+        this.login(result.text);
+      });
+    });
+  }
+
+  login (key) {
+    this.auth.login(key).subscribe(user =>  {
+      this.toastCtrl.create({
+        message: `Bonjour ${user.firstname} ${user.lastname}, vous êtes connecté`,
+        duration: 3000,
+        position: 'top',
+        cssClass: 'toast-success'
+      }).present();
+      console.log(user);
+    }, error => {
+      this.toastCtrl.create({
+        message: 'Problème de connexion, veuillez ressayer plus tard',
+        position: 'top',
+        showCloseButton: true,
+        cssClass: 'toast-fail',
+      }).present();
+      console.error(error);
+    });
+  }
+
+}
