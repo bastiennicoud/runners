fc52199ba4193c196c3536c9ce0a55084781779d
KALMAN Christophe <Christophe.KALMAN@cpnv.ch>
2017-01-26 21:26:29 +0100

    load runs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RunService } from '../../services/run.services';
+import { Run } from '../../models/run';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) { }
+  runs:Run[] = null;
+
+  constructor(public navCtrl: NavController, public runServices: RunService) {
+  }
+
+  ngOnInit(): void {
+    this.runServices.all().subscribe((runs) =>{
+      console.log(runs);
+      this.runs = runs;
+    }, (error) => {
+        console.error("Runs : ", error);
+    });
+  }
 
 }
