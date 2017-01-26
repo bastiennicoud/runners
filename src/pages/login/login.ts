569f46b9199c90716e6a58f5f74ff83cb0d6ac33
Nolan Rigo <nolan.rigo@cpnv.ch>
2017-01-26 17:44:57 +0100

    Remove unused imports

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, Platform, ToastController } from 'ionic-angular';
 import { BarcodeScanner } from 'ionic-native';
 import { AuthService } from '../../services/auth.service';
