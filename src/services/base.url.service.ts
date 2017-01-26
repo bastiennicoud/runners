c6377358fc0f7ccf3050a8c26a9d8beac1f6eac7
Nolan Rigo <nolan.rigo@cpnv.ch>
2017-01-26 20:54:58 +0100

    Change condition for detect dev environment and mobile

--- src/services/base.url.service.ts
+++ src/services/base.url.service.ts
@@ -5,10 +5,10 @@ export abstract class BaseUrlService {
   public BASE_URL: string = '';
 
   constructor(public platform: Platform) {
-    if (platform.is('core')) {
-      this.BASE_URL = '/api'
-    } else {
+    if (platform.is('cordova')) {
       this.BASE_URL = 'https://markal.servehttp.com/runners/api'
+    } else {
+      this.BASE_URL = '/api';
     }
   }
 
