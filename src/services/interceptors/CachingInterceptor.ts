05b1718ec559d96454ba85bd41d59307596e3d9b
Thomas Ricci <grisou13@hotmail.fr>
2017-12-12 16:50:51 +0100

    argl, rxjs

--- src/services/interceptors/CachingInterceptor.ts
+++ src/services/interceptors/CachingInterceptor.ts
@@ -19,30 +19,44 @@ export class CachingInterceptor implements HttpInterceptor {
       return next.handle(req);
     }
     this.cache = this.injector.get(CacheService)
-
     // This will be an Observable of the cached value if there is one,
     // or an empty Observable otherwise. It starts out empty.
     let maybeCachedResponse: Observable<HttpEvent<any>> = Observable.empty();
 
     // Check the cache.
-    this.cache.getItem(req.urlWithParams).then((cachedResponse)=>{
-      console.log("getting from cache")
+    this.cache.getItem(req.url)
+      .then((cachedResponse)=>{
+        console.log("getting from cache : "+req.url)
+        console.debug(cachedResponse)
         maybeCachedResponse = Observable.of(cachedResponse);
-    })
+      })
       .catch(()=>{
-        console.log("No cache entry for : "+req.urlWithParams)
+        console.log("No cache entry for : "+req.url)
       });
-
 
     // Create an Observable (but don't subscribe) that represents making
     // the network request and caching the value.
     const networkResponse = next.handle(req).do(event => {
       // Just like before, check for the HttpResponse event and cache it.
       if (event instanceof HttpResponse) {
-        console.log("saving to cache")
-        this.cache.saveItem(req.urlWithParams, event);
+        this.cache.saveItem(req.url, event);
       }
-    });
+    })
+      .catch((err: HttpErrorResponse) => {
+        if (err.status >= 200 && err.status < 300) {
+          const res = new HttpResponse({
+            body: null,
+            headers: err.headers,
+            status: err.status,
+            statusText: err.statusText,
+            url: err.url
+          });
+
+          return Observable.of(res);
+        } else {
+          return Observable.throw(err);
+        }
+      });;
 
     // Now, combine the two and send the cached response first (if there is
     // one), and the network response second.
--- src/services/run.service.ts
+++ src/services/run.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 // import { HttpService } from './http.service';
 import { Run } from '../models/run';
+import {CacheService} from "ionic-cache";
 export {Run};
 /**
  * Allows you to retrieve or modify the status of a run.
@@ -13,8 +14,31 @@ export {Run};
 @Injectable()
 export class RunService {
 
-  constructor(private httpService: HttpClient) {}
+  constructor(private httpService: HttpClient, private cacheService : CacheService) {}
+  protected saveRun(run : Run) : void {
+    run.runners.forEach(runner => {
+      this.cacheService.saveItem(`runners-${runner.id}`, runner)
+      if(runner.vehicle)
+        this.cacheService.saveItem(`vehicles-${runner.vehicle.id}`, runner.vehicle)
+    })
+  }
+  protected saveRunList(runs: Run[]) : void{
+    this.cacheService.saveItem("runs",runs);
+    runs.forEach(run => {
+      this.cacheService.saveItem(`runs-${run.id}`, run)
+      this.saveRun(run)
+    })
+  }
+  protected getListFromCache(key : string){
+    const cached$ = Observable.fromPromise(this.cacheService.getItem(key))
 
+    return cached$
+      .merge(
+        this.httpService.get('endpoint')
+          .do(result => this.cacheService.saveItem('key', result))
+      )
+      .distinctUntilChanged()
+  }
 /**
  * List all run
  *
@@ -29,7 +53,8 @@ export class RunService {
       .map(runs => runs.map(run => {
         if(run.missingUsers())
           return run;
-      }));
+      }))
+      // .do(runs => this.saveRunList(runs));
   }
 
   /**
@@ -41,10 +66,57 @@ export class RunService {
    * @memberOf RunService
    */
   get(id: string): Observable<Run> {
-    return this.httpService
-      .get<any>(`/runs/${id}`)
 
-      .map(data => Run.build(data));
+    var maybe: Observable<Run> = Observable.empty()
+
+    let normal: Observable<Run> = this.httpService
+      .get<any>(`/runs/${id}`)
+      .map(data => Run.build(data))
+      .flatMap((run:Run) => {
+        if(run == null)
+          this.all()
+            .do(runs => console.log(runs))
+            .map(runs => runs.filter(run => run.id == id))
+            .do(runs => console.log(runs))
+            .map(runs => runs.length ? runs[0] : null)
+            .do(d => console.log(d))
+        else
+          return run
+            // .subscribe(r => run = r, (err) => console.log(err), () => console.log("finished"))
+      })
+
+      /*.catch((err) => {
+
+        console.log(err)
+        if (err instanceof HttpErrorResponse && !this.cacheService.isOnline()) {
+          console.log("AHBAMNSDASNDBMASD")
+          console.log(maybe)
+          maybe.publish
+          this.all()
+            .do(runs => console.log(runs))
+            .map(runs => runs.filter(run => run.id == id))
+            .do(runs => console.log(runs))
+            .map(runs => runs.length ? runs[0] : null)
+            .do(d => console.log(d))
+            .subscribe(run => maybe = Observable.of(run), (err) => console.log(err), () => console.log("finished"))
+          console.log(maybe)
+        }
+
+        else
+          Observable.throw(err)
+      })*/
+
+    return normal.merge(maybe)
+    /*return Observable.concat(
+      this.httpService
+        .get<any>(`/runs/${id}`)
+        .map(data => Run.build(data)),
+      this.all()
+          .do(runs => console.log(runs))
+          .map(runs => runs.filter(run => run.id == id))
+          .do(runs => console.log(runs))
+          .map(runs => runs.length ? runs[0] : null)
+    )*/
   }
 
   /**
