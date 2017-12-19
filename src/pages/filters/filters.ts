541825b640a5ce68fb85264685c54ddbbafa345d
Doran Kayoumi <dorankayoumi@gmail.com>
2017-12-19 16:20:54 +0100

    Created modal for filters

---
+++ src/pages/filters/filters.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core'
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ViewController,
+} from 'ionic-angular'
+
+import { filters, FilterObject } from '../../utils/filterengine/filterEngine'
+
+@IonicPage()
+@Component({
+  selector: 'page-filters',
+  templateUrl: 'filters.html',
+})
+export class FiltersPage {
+  filters: FilterObject = filters
+  filterkeys: string[] = Object.keys(this.filters)
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private viewCtrl: ViewController
+  ) {}
+
+  close() {
+    this.viewCtrl.dismiss()
+  }
+}
