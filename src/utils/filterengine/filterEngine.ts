dd6d931914bfe83ac86ca362e6be4966b32f4437
esteban.sotillo <westixy@gmail.com>
2018-01-31 09:21:41 +0100

    feat(runs): add filter for todays runs

--- src/utils/filterengine/filterEngine.ts
+++ src/utils/filterengine/filterEngine.ts
@@ -1,36 +1,33 @@
-import FilterEngine from "./engine";
-import Filter from "./filter";
+import FilterEngine from './engine'
+import Filter from './filter'
 
-import { RunStatusEnum } from "../../enums/run-status.enum";
+import { RunStatusEnum } from '../../enums/run-status.enum'
+
+import moment from 'moment'
+import { filter } from 'ionic-native/node_modules/rxjs/operator/filter'
 
 export interface FilterObject {
-  [key: string]: Filter;
+  [key: string]: Filter
 }
 
 export const filters: FilterObject = {
-  all: new Filter(a => true, "all"),
-  hideNotReady: new Filter(a => a.ready, "Not ready"),
-  hideCompleted: new Filter(a => a.completed, "Completed"),
+  hideNotReady: new Filter(a => a.ready, 'Not ready'),
+  hideCompleted: new Filter(a => a.completed, 'Completed'),
   mine: new Filter(function(a) {
-    const user = this.externalData;
-    return user.belongsToRun(a);
-  }, "Mine"),
-  urgent: new Filter(a => a.problem, "Urgent")
-};
+    const user = this.externalData
+    return user.belongsToRun(a)
+  }, 'Mine'),
+  urgent: new Filter(a => a.problem, 'Urgent'),
+  today: new Filter(
+    a =>
+      moment(a.beginAt).isBetween(
+        moment().subtract(12, 'h'),
+        moment().add(24, 'h')
+      ),
+    'Today'
+  ),
+}
 
-filters.all.onEnable = () => {
-  for (let key in filters) {
-    if (key === "all") continue;
-    filters[key].enable();
-  }
-};
-filters.all.onDisable = () => {
-  for (let key in filters) {
-    if (key === "all") continue;
-    filters[key].disable();
-  }
-};
+export const filterEngine = new FilterEngine()
 
-export const filterEngine = new FilterEngine();
-
-filterEngine.filters = Object.keys(filters).map(key => filters[key]);
+filterEngine.filters = Object.keys(filters).map(key => filters[key])
