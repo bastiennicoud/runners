bf8edf9f07ca089bb2633c7e47fe383b91871092
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-02-07 08:55:40 +0100

    Implemented driver list

--- src/pages/drivers/drivers.ts
+++ src/pages/drivers/drivers.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core'
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController,
+} from 'ionic-angular'
+import { UserService } from '../../services/user.service'
+import { User } from '../../models/user'
+import { DriverPage } from '../driver/driver'
 
 /**
  * Generated class for the DriversPage page.
@@ -14,12 +22,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'drivers.html',
 })
 export class DriversPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  drivers: User[] = []
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private userService: UserService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DriversPage');
+    const loader = this.loadingCtrl.create({ content: 'Chargement ...' })
+    loader.present().then(() => {
+      this.loadDrivers().subscribe(
+        () => loader.dismiss().catch(err => console.log(err)),
+        err =>
+          err.status != 401 && loader.dismiss().catch(err => console.log(err))
+      )
+    })
   }
 
+  getDrivers() {
+    return this.drivers
+  }
+
+  loadDrivers() {
+    return this.userService.all().do(drivers => (this.drivers = drivers))
+  }
+
+  /**
+   * Function to refresh when pulled
+   *
+   * @param {any} refresher
+   *
+   * @memberOf DriversPage
+   */
+  refreshDrivers(refresher) {
+    this.loadDrivers().subscribe(
+      null,
+      err =>
+        err.status != 401 && refresher.cancel()
+          ? refresher.cancel().catch(err => console.log(err))
+          : true,
+      () => refresher.complete()
+    )
+  }
+
+  /**
+   * Show the detail of a driver
+   *
+   * @param {Driver} { id }
+   *
+   * @memberOf DriversPage
+   */
+  showRun({ id }: User): void {
+    this.navCtrl.push(DriverPage, { id })
+  }
 }
