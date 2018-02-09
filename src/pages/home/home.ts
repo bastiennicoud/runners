7bd51f32b5200280206e423e9d9b777520a7f3d9
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-02-09 14:41:20 +0100

    Moved last update to bottom of bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import {IonicPage, MenuController, NavController, NavParams} from 'ionic-angular';
-import {Subscription} from "rxjs/Rx";
-import {LoginPage} from "../login/login";
-import {AuthService} from "../../services/auth.service";
-import {VehiclesPage} from "../vehicles/vehicles";
-import {SplashPage} from "../splash/splash";
-import {RunsPage} from "../runs/runs";
-import {InternetStatusProvider} from "../../providers/internet-status/internet-status";
-import {SettingsPage} from "../settings/settings";
-import {CacheProvider} from "../../providers/cache/cache";
+import { Component } from '@angular/core'
+import {
+  IonicPage,
+  MenuController,
+  NavController,
+  NavParams,
+} from 'ionic-angular'
+import { Subscription } from 'rxjs/Rx'
+import { LoginPage } from '../login/login'
+import { AuthService } from '../../services/auth.service'
+import { VehiclesPage } from '../vehicles/vehicles'
+import { SplashPage } from '../splash/splash'
+import { RunsPage } from '../runs/runs'
+import { InternetStatusProvider } from '../../providers/internet-status/internet-status'
+import { SettingsPage } from '../settings/settings'
+import { CacheProvider } from '../../providers/cache/cache'
 
 /**
  * Generated class for the HomePage page.
@@ -23,45 +28,71 @@ import {CacheProvider} from "../../providers/cache/cache";
   templateUrl: 'home.html',
 })
 export class HomePage {
-  private lastRefresh:Date;
+  private lastRefresh: Date
 
-  public currentPage:number;
-  public currentComponent:any;
-  public pageName:any = false;
+  public currentPage: number
+  public currentComponent: any
+  public pageName: any = false
   pages = [
-    {id : 0, menu:"Runs", title:"Board", icon: "ios-clipboard-outline", component: RunsPage},
-    {id : 1, menu:"Vehicles", title:"Vehicles", icon:"ios-car", component: VehiclesPage},
-    {id : 2, menu:"Logout", title:"Logout", icon:"log-out", class: "button--bottom"},
-    {id : 3, menu:"Settings", title:"Settings", icon:"settings", component: SettingsPage, class: "button--bottom"},
-  ];
+    {
+      id: 0,
+      menu: 'Runs',
+      title: 'Board',
+      icon: 'ios-clipboard-outline',
+      component: RunsPage,
+    },
+    {
+      id: 1,
+      menu: 'Vehicles',
+      title: 'Vehicles',
+      icon: 'ios-car',
+      component: VehiclesPage,
+    },
+    {
+      id: 2,
+      menu: 'Logout',
+      title: 'Logout',
+      icon: 'log-out',
+      class: 'button--bottom',
+    },
+    {
+      id: 3,
+      menu: 'Settings',
+      title: 'Settings',
+      icon: 'settings',
+      component: SettingsPage,
+      class: 'button--bottom button--last',
+    },
+  ]
 
-  loggedSubscriber: Subscription;
-  refreshSubscriber: Subscription;
+  loggedSubscriber: Subscription
+  refreshSubscriber: Subscription
 
-  constructor(private navCtrl: NavController,
-              private authService: AuthService,
-              private InternetStatus: InternetStatusProvider,
-              public menuCtrl : MenuController,
-              public navParams : NavParams,
-              private cache : CacheProvider) {
-    this.openPage(0);
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthService,
+    private InternetStatus: InternetStatusProvider,
+    public menuCtrl: MenuController,
+    public navParams: NavParams,
+    private cache: CacheProvider
+  ) {
+    this.openPage(0)
   }
 
-  openPage(index : number) {
-
+  openPage(index: number) {
     // check if logout button
     if (index == 2) {
       this.authService.logout()
       return
     }
 
-    this.currentPage = index;
+    this.currentPage = index
     this.currentComponent = this.pages[index]
   }
 
   ionViewWillEnter() {
-    this.pageName = this.navParams.get("title")
-    this.cache.getRefreshChange().subscribe(d => this.lastRefresh = d)
+    this.pageName = this.navParams.get('title')
+    this.cache.getRefreshChange().subscribe(d => (this.lastRefresh = d))
   }
   /**
    * When the auth service tells us the user isn't authenticate, we redirect the user to the login page
@@ -69,9 +100,12 @@ export class HomePage {
    * @memberOf TabsPage
    */
   ionViewWillLoad() {
-    this.loggedSubscriber = this.authService.loggedOut.subscribe(() => this.navCtrl.setRoot(LoginPage));
-    this.refreshSubscriber = this.cache.getRefreshChange().subscribe((d)=>this.lastRefresh = d);
-
+    this.loggedSubscriber = this.authService.loggedOut.subscribe(() =>
+      this.navCtrl.setRoot(LoginPage)
+    )
+    this.refreshSubscriber = this.cache
+      .getRefreshChange()
+      .subscribe(d => (this.lastRefresh = d))
   }
 
   /**
@@ -81,7 +115,6 @@ export class HomePage {
    * @memberOf TabsPage
    */
   ionViewWillLeave() {
-    this.loggedSubscriber && this.loggedSubscriber.unsubscribe();
+    this.loggedSubscriber && this.loggedSubscriber.unsubscribe()
   }
-
 }
